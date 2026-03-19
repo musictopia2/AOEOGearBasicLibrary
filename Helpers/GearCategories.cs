9213873a6cd645aa650d571fc2898b6b4d312d2f
Andy Walter <musictopia2@hotmail.com>
2026-03-18 17:37:02 -0700

    Add project files.

---
+++ Helpers/GearCategories.cs
@@ -0,0 +1,39 @@
+﻿namespace AOEOGearBasicLibrary.Helpers;
+public static class GearCategories
+{
+    public const string ArmorBuilding = "ArmorBuilding";
+    public const string ArmorCloth = "ArmorCloth";
+    public const string ArmorLgt = "ArmorLgt";
+    public const string ArmorMed = "ArmorMed";
+    public const string ArmorPlt = "ArmorPlt";
+    public const string Arrows = "Arrows";
+    public const string BallistaArms = "BallistaArms";
+    public const string Banner = "Banner";
+    public const string BellyBows = "BellyBows";
+    public const string Bows = "Bows";
+    public const string Clubs2H = "Clubs2H";
+    public const string FireThrowers = "FireThrowers";
+    public const string FishingNet = "FishingNet";
+    public const string Gear = "Gear";
+    public const string GearBldg = "GearBldg";
+    public const string GearBoat = "GearBoat";
+    public const string GearPriest = "GearPriest";
+    public const string GearSiege = "GearSiege";
+    public const string GearVill = "GearVill";
+    public const string GreatAxe = "GreatAxe";
+    public const string Javalins = "Javalins";
+    public const string Merchant = "Merchant";
+    public const string RamHeads = "RamHeads";
+    public const string Scepter = "Scepter";
+    public const string ScoutSpecial1H = "ScoutSpecial1H";
+    public const string Shields = "Shields";
+    public const string Sling = "Sling";
+    public const string Spears1H = "Spears1H";
+    public const string Spears2H = "Spears2H";
+    public const string Staffs2H = "Staffs2H";
+    public const string Swords1H = "Swords1H";
+    public const string Tools = "Tools";
+    public const string Torc = "Torc";
+    public const string WarHorn = "War Horn";
+    public const string Warpaint = "Warpaint";
+}
